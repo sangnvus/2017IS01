ceaff7529740559319f12558cef6f7049b404b95
DuongLV <water.fire6789@gmail.com>
2017-02-08 09:52:01 +0700

    commit DB

---
+++ WIP/Users/DuongLV/DB/Dump20170208-1/cex_dev_province.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cex_dev
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `province`
+--
+
+DROP TABLE IF EXISTS `province`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `province` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Code` varchar(10) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Type` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `province`
+--
+
+LOCK TABLES `province` WRITE;
+/*!40000 ALTER TABLE `province` DISABLE KEYS */;
+INSERT INTO `province` VALUES (1,'01','Hà Nội','Thành Phố'),(2,'02','Hà Giang','Tỉnh'),(3,'03','Cao Bằng','Tỉnh'),(4,'04','Bắc Kạn','Tỉnh'),(5,'05','Tuyên Quang','Tỉnh'),(6,'06','Lào Cai','Tỉnh'),(7,'07','Điện Biên','Tỉnh'),(8,'08','Lai Châu','Tỉnh'),(9,'09','Sơn La','Tỉnh'),(10,'10','Yên Bái','Tỉnh'),(11,'11','Hòa Bình','Tỉnh'),(12,'12','Thái Nguyên','Tỉnh'),(13,'13','Lạng Sơn','Tỉnh'),(14,'14','Quảng Ninh','Tỉnh'),(15,'15','Bắc Giang','Tỉnh'),(16,'16','Phú Thọ','Tỉnh'),(17,'17','Vĩnh Phúc','Tỉnh'),(18,'18','Bắc Ninh','Tỉnh'),(19,'19','Hải Dương','Tỉnh'),(20,'20','Hải Phòng','Thành Phố'),(21,'21','Hưng Yên','Tỉnh'),(22,'22','Thái Bình','Tỉnh'),(23,'23','Hà Nam','Tỉnh'),(24,'24','Nam Định','Tỉnh'),(25,'25','Ninh Bình','Tỉnh'),(26,'26','Thanh Hóa','Tỉnh'),(27,'27','Nghệ An','Tỉnh'),(28,'28','Hà Tĩnh','Tỉnh'),(29,'29','Quảng Bình','Tỉnh'),(30,'30','Quảng Trị','Tỉnh'),(31,'31','Thừa Thiên Huế','Tỉnh'),(32,'32','Đà Nẵng','Thành Phố'),(33,'33','Quảng Nam','Tỉnh'),(34,'34','Quảng Ngãi','Tỉnh'),(35,'35','Bình Định','Tỉnh'),(36,'36','Phú Yên','Tỉnh'),(37,'37','Khánh Hòa','Tỉnh'),(38,'38','Ninh Thuận','Tỉnh'),(39,'39','Bình Thuận','Tỉnh'),(40,'40','Kon Tum','Tỉnh'),(41,'41','Gia Lai','Tỉnh'),(42,'42','Đắk Lắk','Tỉnh'),(43,'43','Đắk Nông','Tỉnh'),(44,'44','Lâm Đồng','Tỉnh'),(45,'45','Bình Phước','Tỉnh'),(46,'46','Tây Ninh','Tỉnh'),(47,'47','Bình Dương','Tỉnh'),(48,'48','Đồng Nai','Tỉnh'),(49,'49','Bà Rịa - Vũng Tàu','Tỉnh'),(50,'50','Hồ Chí Minh','Thành Phố'),(51,'51','Long An','Tỉnh'),(52,'52','Tiền Giang','Tỉnh'),(53,'53','Bến Tre','Tỉnh'),(54,'54','Trà Vinh','Tỉnh'),(55,'55','Vĩnh Long','Tỉnh'),(56,'56','Đồng Tháp','Tỉnh'),(57,'57','An Giang','Tỉnh'),(58,'58','Kiên Giang','Tỉnh'),(59,'59','Cần Thơ','Thành Phố'),(60,'60','Hậu Giang','Tỉnh'),(61,'61','Sóc Trăng','Tỉnh'),(62,'62','Bạc Liêu','Tỉnh'),(63,'63','Cà Mau','Tỉnh');
+/*!40000 ALTER TABLE `province` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-08  9:49:23
